7e6dd05bf89f1b7253cb1fa997a0be2f795fcdc7
wangkunkey <wangkunkey@163.com>
2019-12-21 21:15:32 +0700

    修改文件了

--- src/main/java/com/zallsoon/GitMainTest.java
+++ src/main/java/com/zallsoon/GitMainTest.java
@@ -3,6 +3,7 @@ package com.zallsoon;
 public class GitMainTest {
     public static void main(String[] args) {
         System.out.println("This is Test");
+        System.out.println("CCCCCC");
     }
 
 }
